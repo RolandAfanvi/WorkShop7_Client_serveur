55abc3375a1af5754f4513eb5ad4b4ae9a0c3112
RolandAfanvi <125564906+RolandAfanvi@users.noreply.github.com>
2024-02-19 16:00:27 +0100

    Add files to the repository

---
+++ WS_Socket/Client/client.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    private static Socket SeConnecter()
+    {
+        Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        int port = 11000;
+        IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, port);
+        clientSocket.Connect(ipEndPoint);
+        return clientSocket;
+    }
+
+    private static void EcouterReseau(Socket clientSocket)
+    {
+        byte[] buffer = new byte[1024];
+        int bytesReceived;
+        while ((bytesReceived = clientSocket.Receive(buffer)) > 0)
+        {
+            string message = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
+            Console.WriteLine($"Message reçu du serveur: {message}");
+            byte[] response = Encoding.ASCII.GetBytes("Message reçu");
+            clientSocket.Send(response);
+        }
+    }
+
+    private static void Deconnecter(Socket socket)
+    {
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
+    }
+
+    static void Main(string[] args)
+    {
+        Socket clientSocket = SeConnecter();
+        EcouterReseau(clientSocket);
+        Deconnecter(clientSocket);
+    }
+}
---
+++ WS_Socket/Server/server.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    private static Socket SeConnecter()
+    {
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        int port = 11000;
+        IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, port);
+        serverSocket.Bind(ipEndPoint);
+        serverSocket.Listen(10);
+        return serverSocket;
+    }
+
+    private static Socket AccepterConnexion(Socket serverSocket)
+    {
+        Socket clientSocket = serverSocket.Accept();
+        IPEndPoint clientEndPoint = clientSocket.RemoteEndPoint as IPEndPoint;
+        Console.WriteLine($"Client connecté: {clientEndPoint.Address}:{clientEndPoint.Port}");
+        return clientSocket;
+    }
+
+    private static void EcouterReseau(Socket clientSocket)
+    {
+        byte[] buffer = new byte[1024];
+        int bytesReceived;
+        while ((bytesReceived = clientSocket.Receive(buffer)) > 0)
+        {
+            string message = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
+            Console.WriteLine($"Message reçu du client: {message}");
+            byte[] response = Encoding.ASCII.GetBytes("Message reçu");
+            clientSocket.Send(response);
+        }
+    }
+
+    private static void Deconnecter(Socket socket)
+    {
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
+    }
+
+    static void Main(string[] args)
+    {
+        Socket serverSocket = SeConnecter();
+        Socket clientSocket = AccepterConnexion(serverSocket);
+        EcouterReseau(clientSocket);
+        Deconnecter(clientSocket);
+        Deconnecter(serverSocket);
+    }
+}
